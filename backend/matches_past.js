b80b5ced75c00496211605f195fe1f8205f8f9dd
Charaf ZELLOU <charaf.zellou@smart-chain.fr>
2022-09-29 01:31:04 +0200

    Updates following review

--- backend/matches_past.js
+++ backend/matches_past.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
 const sdk = require('api')('@developers-pandascore/v2#apvn9q1ml4sw7gei');
-import * as dotenv from 'dotenv'
+const dotenv = require('dotenv')
 
 dotenv.config(({path:__dirname+'/.env'}))
 
 sdk.auth(process.env.APIKEY);
 
-sdk['get_matches_past']({
+sdk.get_matches_past({
     'filter[finished]': 'true',
     'filter[not_started]': 'false',
     sort: '',
     page: '1',
     per_page: '1'
 })
-    .then(res => console.log(res))
+    .then(res => {
+        console.log(res);
+        let data = JSON.stringify(res);
+        fs.writeFileSync(__dirname + '/matches_past.json', data);
+    })
     .catch(err => console.error(err));
